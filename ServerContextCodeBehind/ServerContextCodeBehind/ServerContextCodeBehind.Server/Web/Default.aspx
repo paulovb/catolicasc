f584a545fa3f6e6be45c9582a858d36cb901e2b5
paulovb <paulo.bischof@gmail.com>
2013-10-01 22:59:05 -0300

    new

---
+++ ServerContextCodeBehind/ServerContextCodeBehind/ServerContextCodeBehind.Server/Web/Default.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LightSwitchApplication.Web.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Phone Calls</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <style type="text/css">
+                .style1
+                {
+                    font-weight: normal;
+                }
+            </style>
+            <asp:DataList ID="dlPhoneCalls" runat="server" GridLines="Both">
+                <HeaderTemplate>
+                    &nbsp;</td><td>
+                        <strong>Description</strong> </td>
+                    <td>
+                        <strong>Person Calling</strong>
+                    </td>
+                    <td>
+                        <strong>Phone Number</strong>
+                    </td>
+                    <td>
+                        <strong>Call Type</strong>
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete"
+                        CommandArgument='<%# Eval("Id") %>' OnClick="btnDelete_Click" />
+                    <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click"
+                        Text="Update" CommandArgument='<%# Eval("Id") %>' />
+                    </td><td>
+                        <asp:TextBox ID="Description" runat="server" Text='<%# Eval("Description") %>' />
+                    </td>
+                    <td>
+                        <asp:TextBox ID="PersonCalling" runat="server" Text='<%# Eval("PersonCalling") %>' />
+                    </td>
+                    <td>
+                        <asp:TextBox ID="PhoneNumber" runat="server" Text='<%# Eval("PhoneNumber") %>' />
+                    </td>
+                    <td>
+                        <asp:Label ID="lblCallType" runat="server" Text='<%# Eval("CallType") %>' Visible="False" />
+                        <asp:DropDownList ID="ddlGridCallType" runat="server"
+                            OnDataBound="ddlCallType_DataBound">
+                            <asp:ListItem>Sales</asp:ListItem>
+                            <asp:ListItem>Service</asp:ListItem>
+                            <asp:ListItem>Other</asp:ListItem>
+                        </asp:DropDownList>
+                </ItemTemplate>
+            </asp:DataList>
+            <p>
+                &nbsp;
+            </p>
+            <h2 class="style1">Insert New:</h2>
+            <table>
+                <tr>
+                    <td align="right">
+                        <strong>Description</strong>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtDescription" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        <strong>Person Calling</strong>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPersonCalling" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        <strong>Phone Number</strong>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtPhoneNumber" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        <strong>Call Type</strong>
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddlCallType" runat="server">
+                            <asp:ListItem>Sales</asp:ListItem>
+                            <asp:ListItem>Service</asp:ListItem>
+                            <asp:ListItem>Other</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">&nbsp;
+                    </td>
+                    <td align="right">
+                        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+                    </td>
+                </tr>
+            </table>
+            <asp:Label ID="lblError" runat="server" EnableViewState="False" ForeColor="Red"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ ServerContextCodeBehind/ServerContextCodeBehind/ServerContextCodeBehind.Server/Web/Default.aspx.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+
+namespace LightSwitchApplication.Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                RefreshGrid();
+            }
+        }
+
+        private void RefreshGrid()
+        {
+            // Get all PhoneCalls
+            using (var serverContext = GetServerContext())
+            {
+                var result = from PhoneCalls in serverContext.DataWorkspace.ApplicationData
+                                 .PhoneCalls.GetQuery().Execute()
+                             select PhoneCalls;
+
+                dlPhoneCalls.DataSource = result;
+                dlPhoneCalls.DataBind();
+            }
+        }
+
+        protected void ddlCallType_DataBound(object sender, EventArgs e)
+        {
+            // Get an instance of the DropDownList
+            DropDownList objDropDownList = (DropDownList)sender;
+            // Get the selected value from the hidden Label
+            Label objLabel = (Label)objDropDownList.Parent.FindControl("lblCallType");
+            // Set the selected value in the DropDownList
+            objDropDownList.SelectedValue = objLabel.Text;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            using (var serverContext = GetServerContext())
+            {
+                try
+                {
+                    // Create a new PhoneCall
+                    var objPhoneCall = serverContext.DataWorkspace.ApplicationData.PhoneCalls.AddNew();
+
+                    // Set values
+                    objPhoneCall.CallType = ddlCallType.SelectedValue;
+                    objPhoneCall.Description = txtDescription.Text;
+                    objPhoneCall.PersonCalling = txtPersonCalling.Text;
+                    objPhoneCall.PhoneNumber = txtPhoneNumber.Text;
+                    objPhoneCall.MessageTakenBy = 
+                        (this.User.Identity.Name != "") ? this.User.Identity.Name : "[unknown]";
+
+                    // Save changes
+                    serverContext.DataWorkspace.ApplicationData.SaveChanges();
+
+                    // Refresh the Grid
+                    RefreshGrid();
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                    return;
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Get an instance of the Button
+            Button UpdateButton = (Button)sender;
+            // Get the ID of the current record from the CommandArgument
+            int intID = Convert.ToInt32(UpdateButton.CommandArgument);
+
+            using (var serverContext = GetServerContext())
+            {
+                try
+                {
+                    // Get the record
+                    var result = (from PhoneCalls in serverContext.DataWorkspace.ApplicationData
+                                 .PhoneCalls.GetQuery().Execute()
+                                  where PhoneCalls.Id == intID
+                                  select PhoneCalls).FirstOrDefault();
+
+                    if (result != null)
+                    {
+                        // Get the values
+                        TextBox Description =
+                            (TextBox)UpdateButton.Parent.FindControl("Description");
+                        TextBox PersonCalling =
+                            (TextBox)UpdateButton.Parent.FindControl("PersonCalling");
+                        TextBox PhoneNumber =
+                            (TextBox)UpdateButton.Parent.FindControl("PhoneNumber");
+                        DropDownList ddlGridCallType =
+                            (DropDownList)UpdateButton.Parent.FindControl("ddlGridCallType");
+                        
+                        // Update the record
+                        result.Description = Description.Text;
+                        result.PersonCalling = PersonCalling.Text;
+                        result.PhoneNumber = PhoneNumber.Text;
+                        result.CallType = ddlGridCallType.SelectedValue;
+
+                        // Save changes
+                        serverContext.DataWorkspace.ApplicationData.SaveChanges();
+
+                        // Refresh the Grid
+                        RefreshGrid();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                    return;
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Get an instance of the Button
+            Button DeleteButton = (Button)sender;
+            // Get the ID of the current record from the CommandArgument
+            int intID = Convert.ToInt32(DeleteButton.CommandArgument);
+
+            using (var serverContext = GetServerContext())
+            {
+                try
+                {
+                    // Get the record
+                    var result = (from PhoneCalls in serverContext.DataWorkspace.ApplicationData
+                                 .PhoneCalls.GetQuery().Execute()
+                                  where PhoneCalls.Id == intID
+                                  select PhoneCalls).FirstOrDefault();
+
+                    if (result != null)
+                    {
+                        // Delete the record
+                        result.Delete();
+
+                        // Save changes
+                        serverContext.DataWorkspace.ApplicationData.SaveChanges();
+
+                        // Refresh the Grid
+                        RefreshGrid();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                    return;
+                }
+            }
+        }
+
+        // Utility
+
+        #region GetServerContext
+        private static ServerApplicationContext GetServerContext()
+        {
+            ServerApplicationContext serverContext = (LightSwitchApplication.ServerApplicationContext)ServerApplicationContext.Current;
+            if (serverContext == null)
+            {
+                serverContext = (LightSwitchApplication.ServerApplicationContext)ServerApplicationContext.CreateContext();
+            }
+            return serverContext;
+        } 
+        #endregion
+
+        #region ShowError(Exception ex)
+        private void ShowError(Exception ex)
+        {
+            string strError = "";
+            Microsoft.LightSwitch.ValidationException ValidationErrors = ex as Microsoft.LightSwitch.ValidationException;
+
+            if (ValidationErrors != null)
+            {
+                StringBuilder sbErrorMessage = new StringBuilder();
+                foreach (var error in ValidationErrors.ValidationResults)
+                {
+                    sbErrorMessage.Append(string.Format("<p>{0}</p>", error.Message));
+                }
+
+                strError = sbErrorMessage.ToString();
+            }
+            else
+            {
+                // This is a simple error -- just show Message
+                strError = ex.Message;
+            }
+
+            // This is a simple error -- just show Message
+            lblError.Text = strError;
+        }
+        #endregion
+
+    }
+}
